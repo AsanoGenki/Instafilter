7d066ce92297f39564292408ae6c6916c5cc21a7
AsanoGenki <asanogenki0218@icloud.com>
2024-08-26 11:31:48 +0900

    add: 基本的なUIの構築

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("写真がありません", systemImage: "photo.badge.plus", description: Text("タップして画像をインポートしてください"))
+                }
+                Spacer()
+                HStack {
+                    Text("強度")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                HStack {
+                    Button("フィルターの変更", action: changeFilter)
+                    Spacer()
+                    // share the picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .padding()
+    }
+    func changeFilter() {
     }
 }
 
