7ecf93b252e1b2ed693d6ef773b0df31d4e02c4d
AsanoGenki <asanogenki0218@icloud.com>
2024-08-26 10:51:29 +0900

    Initial commit.

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Genki on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
